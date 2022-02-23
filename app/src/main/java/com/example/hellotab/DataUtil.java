df22f970930dc8f0e437a106a6b5aae6f327c222
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-02-22 20:11:05 -0800

    Add Default Changelist

--- app/src/main/java/com/example/hellotab/DataUtil.java
+++ app/src/main/java/com/example/hellotab/DataUtil.java
@@ -6,13 +6,64 @@ import java.util.List;
 public class DataUtil {
 
   static List<CategoryEntity> categoryList() {
-    return Arrays.asList(new CategoryEntity("Category 1", student1List()), new CategoryEntity("Category 2",
-        student2List()));
+    return Arrays.asList(
+        new CategoryEntity("Category 1", student1List()),
+        new CategoryEntity("Category 2", student2List()),
+        new CategoryEntity("Category 3", student2List()),
+        new CategoryEntity("Category 4", student2List()),
+        new CategoryEntity("Category 5", student2List())
+    );
   }
   static List<StudentEntity> student1List() {
-    return Arrays.asList(new StudentEntity("11", "Tony11"), new StudentEntity("12", "Jun11"));
+    return Arrays.asList(
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("11", "Tony11"),
+        new StudentEntity("12", "Jun11"));
   }
+
   static List<StudentEntity> student2List() {
-    return Arrays.asList(new StudentEntity("22", "Tony22"), new StudentEntity("23", "Jun22"));
+    return Arrays.asList(
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("22", "Tony22"),
+        new StudentEntity("23", "Jun22"));
   }
 }
